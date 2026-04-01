ae8ad274c6526799809fadc5e6720bf89423da2b
TheKnorx <the.knorx@proton.me>
2026-04-01 20:45:09 +0200

    File with usefull functions to debload the main executable - maybe used in the future

---
+++ helper_functions.lib.asm
@@ -0,0 +1,8 @@
+; This file contains some usefull functions that will be used frequently by the main program
+; We define them here to avoid making the main executable to bloated
+; All functions (must) follow the System V ABI calling conventions
+
+section .text
+
+%include "flags.asm.inc"
+
